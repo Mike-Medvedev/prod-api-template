ef299bbabd2b831c8756d8b49283f40e7293071b
Michael Medvedev <mmedvedev20@gmail.com>
2025-11-26 08:20:52 -0500

    added dev and prod logging setup for winston

--- src/logger/logger.ts
+++ src/logger/logger.ts
@@ -1,10 +1,10 @@
 import { createLogger, format, transports } from 'winston';
 import type { TransformableInfo } from "logform";
-import { requestContext, type RequestContext } from "../context/request.context.ts"
+import { requestContext } from "../context/request.context.ts"
 
 
 
-function loggerFormat(info: TransformableInfo): string {
+function devFormat(info: TransformableInfo): string {
     const { reqId, method, path, body } = requestContext.getStore() ?? {};
     const requestData = `Request Context: ${reqId} ${method} ${path} ${Object.entries(body || {})}`
     const base = `${info.level} ${info.message}\r\n${reqId ? requestData : ""}\r\n`
@@ -13,14 +13,46 @@ function loggerFormat(info: TransformableInfo): string {
     }
     return base
 }
-const logger = createLogger({
-    level: 'info',
-    format: format.combine(
-        format.colorize(),
-        format.printf(loggerFormat)
-    ),
-    transports: new transports.Console()
+
+const addRequestContext = format((info) => {
+    const { reqId, method, path } = requestContext.getStore() ?? {};
+    info = { reqId, method, path, ...info };
+    return info
 });
+
+let winstonLogger = {}
+if (process.env.NODE_ENV !== "production") {
+    winstonLogger = {
+        level: 'info',
+        defaultMeta:{
+            service: "lock-in-api",
+            env: process.env.NODE_ENV ?? 'dev'
+        },
+        format: format.combine(
+            format.colorize(),
+            format.printf(devFormat)
+        ),
+        transports: new transports.Console()
+    }
+}
+else {
+    winstonLogger = {
+        level: 'info',
+        defaultMeta:{
+            service: "lock-in-api",
+            env: process.env.NODE_ENV ?? 'dev'
+        },
+        format: format.combine(
+            format.timestamp(),
+            format.errors({ stack: true }),
+            addRequestContext(),
+            format.json()
+        ),
+        transports: new transports.File({ filename: 'log.log', options: { flags: 'w' } }),
+    }
+}
+
+const logger = createLogger(winstonLogger);
 
 
 export default logger; 
